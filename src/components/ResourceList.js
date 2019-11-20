c7d425fd75db11b5e7a600e63628a6234c488ed6
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2019-11-20 18:07:45 +0200

    little improvement

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
+const ResourceList = ({resource}) => {
     const [resources, setResources] = useState([]);
 
-    const fetchResource = async (resource) => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-        setResources(response.data);
-    };
-
     useEffect(() => {
-        fetchResource(resource);
+        (async resource => {
+            const response = await axios.get(
+                `https://jsonplaceholder.typicode.com/${resource}`
+            );
+
+            setResources(response.data);
+        })(resource);
     }, [resource]);
 
     return (
